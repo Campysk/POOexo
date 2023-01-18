c70e46bdcdd48f4c8ea3d60edd1c4d5d732b08e6
Campysk <e.gay@lprs.fr>
2023-01-18 15:58:32 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    private String titre;
+    private String auteur;
+    private float prix;
+
+    public String settitre(String newTitre){
+        this.titre = newTitre;
+        return this.titre;
+    }
+    public String setauteur(String newAuteur){
+        this.auteur = newAuteur;
+        return this.auteur;
+    }
+    public float setprix(float newPrix){
+        this.prix = newPrix;
+        return this.prix;
+    }
+
+
+
+}
